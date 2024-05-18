9f3e19153a980f887e8456fd272e6c030e4fe6a5
ewooral <boahen.cos@gmail.com>
2024-05-18 15:56:15 +0000

    feat: Consume the rust restapi by accessing the endpoint handler method, greet that accepts parameters, greeting type and a name of the person you're greeting

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,3 @@
-// #![feature(proc_macro_hygiene, decl_macro)]
-
-// #[macro_use] extern crate rocket;
-// extern crate rocket_cors;
-
-// use rocket::http::Method;
-// use rocket::response::content;
-// use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
-
-// #[get("/")]
-// fn index() -> &'static str {
-//     "Hello, world!"
-// }
-
-// #[get("/greet/<greeting>/<name>")]
-// fn greet(greeting: String, name: String) -> String {
-//     format!("Good {}, {}", greeting, name)
-// }
-
-// fn main() {
-//     let (allowed_origins, failed_origins) = AllowedOrigins::some(&[
-//         "http://localhost:3000",
-//         // "http://your_nextjs_app_origin",
-//     ]);
-//     assert!(failed_origins.is_empty());
-
-//     let options = rocket_cors::Cors {
-//         allowed_origins,
-//         allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
-//         allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
-//         allow_credentials: true,
-//         ..Default::default()
-//     };
-
-//     rocket::ignite()
-//         .mount("/", routes![index, greet])
-//         .attach(options)
-//         .launch();
-// }
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
@@ -48,7 +9,7 @@ use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "This is Elijah, my first api project in rust using the rocket framework!"
 }
 
 #[get("/greet/<greeting>/<name>")]
